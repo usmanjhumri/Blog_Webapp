55b54aa3d89fddb8fda9172408905ba59c6f31e3
usmanlatif123 <usman@enigmadevcraft.com>
2023-09-30 09:58:44 +0500

    updated

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { ThemeContextProvider } from "./context/ThemeContext";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeContextProvider>{children}</ThemeContextProvider>
+      </body>
     </html>
   );
 }
